01a9094f02b4a0295a97e72b0d388b7bccbcce0b
TinaLindhqvist <TinaLindhqvist@users.noreply.github.com>
2016-06-16 11:57:24 +0200

    Finalize the test

--- tests/AppTestScript.js
+++ tests/AppTestScript.js
@@ -1,41 +1,34 @@
-console.log('Testing has started');
+'use strict';
+console.log('Testing started');
+
 var webdriver = require('selenium-webdriver'),
     username = process.env.SAUCE_USERNAME,
     accessKey = process.env.SAUCE_ACCESS_KEY,
     driver;
- 
- var url = "http://uitestpipeline.stage1.mybluemix.net/";//process.env.APP_URL;
- 
- // checking sauce credential
-if(!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY){
-    console.warn(
-        '\nPlease configure your sauce credential:\n\n' +
-        'export SAUCE_USERNAME=<SAUCE_USERNAME>\n' +
-        'export SAUCE_ACCESS_KEY=<SAUCE_ACCESS_KEY>\n\n'
-    );
-    throw new Error("Missing sauce credentials");
-}
- 
+
+var url = process.env.APP_URL;
+
+console.log('Testing of the deployed url [' + url + '] is not possible since stage1 is not public');
+
 driver = new webdriver.Builder().
   withCapabilities({
-    'browserName': 'chrome',
-    'platform': 'Windows XP',
+    'browserName': 'firefox',
+    'platform': 'Windows 7',
     'version': '43.0',
     'username': username,
-    'accessKey': accessKey
+    'accessKey': accessKey,
+    'name': 'Deployed App Web page',
+    'build': process.env.BUILD_DATE
   }).
-  usingServer("http://" + username + ":" + accessKey +
-              "@ondemand.saucelabs.com:80/wd/hub").
-  build();
- 
-driver.get(url);
+  usingServer("http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub").build();
 
-console.log('Testing on-going');
 
-driver.getPageSource().contains("Deployed");
-
+console.log('Testing will be performed using deployed page ' + process.env.TEST_URL);
+driver.get(process.env.TEST_URL);
 driver.getTitle().then(function (title) {
     console.log("title is: " + title);
 });
- 
+
+console.log('Testing done');
+
 driver.quit();
